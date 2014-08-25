599e3cfab7f08af57253f29aa9140b4c70101395
William Lindner <wlindner522@gmail.com>
2014-08-25 13:22:28 -0700

    Added model bar chart. Clicking on make filters model.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,10 +2,12 @@
 var apiKey = '53e569eae4b0a9e9da986978',
     host = 'daily.zoomdata.com/zoomdata',
     secure = true,
-    sourceName = "Vehicle Complaints";
+    sourceName = "Vehicle Complaints",
+    makeVis, modelVis;
 
 
-var $makeBarChart = $("#make-bar-chart");
+var $makeBarChart = $("#make-bar-chart"),
+	$modelBarChart = $("#model-bar-chart");
 
 var zoomdataClient = new ZoomdataClient({
     apiKey: apiKey,
@@ -14,15 +16,45 @@ var zoomdataClient = new ZoomdataClient({
 });
 
 zoomdataClient.visualize({
-    visualization: "Vehicle Complaints Horizontal Bars",
+    visualization: "Horizontal Bars by Make",
     source: sourceName,
     element: $makeBarChart.get(0)
 }).done(function(visualization) {
+	makeVis = visualization;
+
     visualization
         .controller
         .elementsManager
         .on('interaction', function (interactiveElement) {
         	var active = $makeBarChart.find('div.active');
+        	active.toggleClass('active', false);
+
+            interactiveElement.$el.toggleClass('active');
+
+            var carModel = interactiveElement.data().group;
+
+		    var filter = {
+		        operation: 'IN',
+		        path: 'make',
+		        value: [carModel]
+		    };
+
+		    modelVis.controller.state.setFilter(filter);
+        });
+});
+
+zoomdataClient.visualize({
+    visualization: "Horizontal Bars by Model",
+    source: sourceName,
+    element: $modelBarChart.get(0)
+}).done(function(visualization) {
+	modelVis = visualization;
+
+    visualization
+        .controller
+        .elementsManager
+        .on('interaction', function (interactiveElement) {
+        	var active = $modelBarChart.find('div.active');
         	active.toggleClass('active', false);
         	
             interactiveElement.$el.toggleClass('active');
