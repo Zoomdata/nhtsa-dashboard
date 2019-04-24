c7693e590998581a2f19dc6abf4a57aa17e1f955
Jonathan Avila <jonathan@zoomdata.com>
2019-04-24 16:24:08 -0400

    Updates

---
+++ precache-manifest.33ad87bd9a441d1416331d35ff0c8696.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe3ec7be81ae6dbd592458f7c507eb30",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f881da8079ae02cc2371",
+    "url": "./static/css/2.4c1998f3.chunk.css"
+  },
+  {
+    "revision": "7dee512b546e7ceed267",
+    "url": "./static/css/main.c919af7b.chunk.css"
+  },
+  {
+    "revision": "f881da8079ae02cc2371",
+    "url": "./static/js/2.8e4765d9.chunk.js"
+  },
+  {
+    "revision": "7dee512b546e7ceed267",
+    "url": "./static/js/main.cc95c967.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "34a563c4e935faddd1198857e19e59f7",
+    "url": "./static/media/2BB2E2_0_0.34a563c4.ttf"
+  },
+  {
+    "revision": "4a4c97faccdd2e7efb236806c90bd159",
+    "url": "./static/media/2BB2E2_0_0.4a4c97fa.woff"
+  },
+  {
+    "revision": "3fbd26c5eed02aab25ada21426aae0bb",
+    "url": "./static/media/car_background@2x.3fbd26c5.jpg"
+  },
+  {
+    "revision": "30fc113b86b7a4a1bde53585eb4be5fd",
+    "url": "./static/media/overlay_splat4.30fc113b.gif"
+  },
+  {
+    "revision": "1b6230b0cca1f8ec5ea278f8a3a03729",
+    "url": "./static/media/pick_a_make.1b6230b0.png"
+  },
+  {
+    "revision": "18a09fa357d948da1f154fc9cbc17506",
+    "url": "./static/media/show-data.18a09fa3.png"
+  }
+]);
